2b038189930a6b16b6ef370aee534679c6bb251a
SMakela <73409799+smakela13@users.noreply.github.com>
2021-04-26 18:23:45 -0400

    Began working on the schema's department, role, and employee tables, also added a JS file to get started with

---
+++ schema.sql
@@ -0,0 +1,30 @@
+-- If a database already exists, it'll be deleted and then created and used.
+DROP DATABASE IF EXISTS companyDB;
+CREATE database companyDB;
+USE companyDB;
+
+-- Creating a table for the department.
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- Creating a table for role.
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+);
+
+-- Creating a table for employee.
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY(id),
+);
